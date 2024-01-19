59ea7741e9e478233e4cd0154ff99fbd0f3d5273
victor.popa <popa.georgiani93@gmail.com>
2024-01-19 22:31:10 +0200

    sync

---
+++ api_sync.py
@@ -0,0 +1,6 @@
+# from playwright.sync_api import sync_playwright
+
+
+print('test')
+# The Sync API takes care of syncing your Contentstack data with
+# your application and ensures that the data is always up-to-date by providing delta updates
